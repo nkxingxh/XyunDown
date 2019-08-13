5cf1319c55f7ed630bd8da6bd31bbddc1443b676
nkxingxh <nkxingxh@nkxingxh.top>
2019-08-13 22:19:54 +0800

    Update footer.php

--- root/views/footer.php
+++ root/views/footer.php
@@ -6,6 +6,8 @@
 		<li><a href="http://blog.nkxingxhtop:8088" target="_blank">站长博客</a></li >
 		<br />
 	</ul>
+	<p>本站所有资源均来源于网络，仅供学习交流使用，禁止用于商业目的！</p>
+	<p>如发现本站资源侵犯了你的版权，可以发送邮件给我，请在邮件中务必附上相关作品的版权证明.</p>
 	<p>Copyright © 2019 XyunDown. All Rights Reserved. </p>
 	<p>Powered By NKXingXh</p>
 	<script src="http://www.admin88.com/mystat.asp?id=52603&logo=11"></script>
